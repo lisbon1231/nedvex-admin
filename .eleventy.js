17a3869fd2f6a35f8cbad6e6918b07f490c10255
Liza Grinenko <xli3kox@gmail.com>
2021-04-09 22:01:12 +0800

    add vacancies sorting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,23 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('static');
 
+    eleventyConfig.addFilter("sortByPriority", arr => {
+        arr.sort((a, b) => {
+            let prior1 = a.data.priority
+            let prior2 = b.data.priority
+            
+            if(prior1 == undefined) {
+                prior1 = 100
+            }
+
+            if(prior2 == undefined) {
+                prior2 = 100
+            }
+            return prior1 > prior2 ? 1 : -1
+        });
+        return arr;
+      });
+
     return {
         dir: {
             layouts: 'templates',
